171b6e7867e5fca26ea53cd39638a6e9a7ab17dc
Techipeeyon <70622125+Techipeeyon@users.noreply.github.com>
2020-10-17 23:50:23 -0700

    completed the project

--- hacks.py
+++ hacks.py
@@ -1,10 +1,7 @@
-#Inputing coefficients of the eqaution
 a = int(input("Enter a : "))
 b = int(input("Enter b : "))
 c = int(input("Enter c : "))
-#calculating the discriminant
 d = ((b**2) - (4*a*c))
-#Calculating the root
 if d > 0:
  d**=0.5
  r1 = (-b+d)/(2*a)
