b771299f0f1a98427107adab9c38a053eb2e0018
Techipeeyon <70622125+Techipeeyon@users.noreply.github.com>
2020-10-17 23:32:52 -0700

    started verifying whether nature of the roots

--- hacks.py
+++ hacks.py
@@ -4,4 +4,11 @@ b = int(input("Enter b : "))
 c = int(input("Enter c : "))
 #calculating the discriminant
 d = ((b**2) - (4*a*c))
+#Calculating the root
+if d > 0:
+ d**=0.5
+ r1 = (-b+d)/(2*a)
+ r2 = (-b-d)/(2*a)
+ print("The roots are real and distinct")
+ print("The roots are : {}, {}".format(r1,r2))
 
